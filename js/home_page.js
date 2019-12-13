bb988b834ee5982487acb799854de94ffe97896d
MaratulyAli <maratulyali01@gmail.com>
2019-12-13 10:35:30 +0500

    Add back to top in external js file

--- js/home_page.js
+++ js/home_page.js
@@ -1,1 +1,6 @@
-let btn = document.querySelector('div#back_to_top');
+let backToTopBtn = document.querySelector('#back_to_top');
+backToTopBtn.addEventListener('click', backToTop);
+function backToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
+}
